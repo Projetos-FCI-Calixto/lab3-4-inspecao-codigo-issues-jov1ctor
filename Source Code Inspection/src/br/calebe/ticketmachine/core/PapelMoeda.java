488f44aa25b832468dbf4466fd02325d34036885
joaomota <125226971+jov1ctor@users.noreply.github.com>
2025-09-01 20:30:19 -0300

    Update PapelMoeda.java

--- Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
@@ -6,11 +6,11 @@ package br.calebe.ticketmachine.core;
  */
 public class PapelMoeda {
 
-    protected int valor;
-    protected int quantidade;
+    protected int valor;  //SUGESTÃO: documentar e padronizar "centavos" para evitar ambiguidade.
+    protected int quantidade; // PROBLEMA: em CSU01, a inserção é de UMA nota por vez.
 
     public PapelMoeda(int valor, int quantidade) {
-        this.valor = valor;
+        this.valor = valor;  //// PROBLEMA: não há validação da denominação nem da quantidade. validar `quantidade >= 1`; em contexto de inserção (CSU01)   
         this.quantidade = quantidade;
     }
 
@@ -22,3 +22,4 @@ public class PapelMoeda {
         return quantidade;
     }
 }
+
